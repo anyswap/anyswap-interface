7f0c0537cfb1533e68534e4262752132816fd2f4
d_winter <2624376436@qq.com>
2021-01-18 19:18:17 +0800

    change language

--- src/components/swap/confirmPriceImpactWithoutFee.ts
+++ src/components/swap/confirmPriceImpactWithoutFee.ts
@@ -1,5 +1,4 @@
 import { Percent } from '@uniswap/sdk'
-import { useTranslation } from 'react-i18next'
 import { ALLOWED_PRICE_IMPACT_HIGH, PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN } from '../../constants'
 
 /**
@@ -8,16 +7,19 @@ import { ALLOWED_PRICE_IMPACT_HIGH, PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN } from 
  * @param priceImpactWithoutFee price impact of the trade without the fee.
  */
 export default function confirmPriceImpactWithoutFee(priceImpactWithoutFee: Percent): boolean {
-  const { t } = useTranslation()
   if (!priceImpactWithoutFee.lessThan(PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN)) {
     return (
       window.prompt(
-        t('tip47', {params: PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN.toFixed( 0 )})
+        `This swap has a price impact of at least ${PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN.toFixed(
+          0
+        )}%. Please type the word "confirm" to continue with this swap.`
       ) === 'confirm'
     )
   } else if (!priceImpactWithoutFee.lessThan(ALLOWED_PRICE_IMPACT_HIGH)) {
     return window.confirm(
-      t('tip48', {params: ALLOWED_PRICE_IMPACT_HIGH.toFixed( 0 )})
+      `This swap has a price impact of at least ${ALLOWED_PRICE_IMPACT_HIGH.toFixed(
+        0
+      )}%. Please confirm that you would like to continue with this swap.`
     )
   }
   return true
