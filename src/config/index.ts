c744c55d973bcd329e370f3669e14b1de2323c58
d_winter <2624376436@qq.com>
2021-01-19 18:51:49 +0800

    add ht main config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,16 @@
 import httest from './tokens/httest.json'
+import htmain from './tokens/htmain.json'
 
 interface ConFig {
   [key: string]: any
 }
-const explorerUrl = 'https://scan-testnet.hecochain.com'
-const netConfig:ConFig = {
+
+const ENV = 0 ? 'main' : 'test'
+
+let explorerUrl = 'https://scan-testnet.hecochain.com'
+let netConfig:ConFig = {
   oldAppName: 'Anyswap V1',
-  appName: 'Anyswap V2',
+  appName: 'HTswap LP',
   name: 'Huobi',
   symbol: 'HT',
   baseCurrency: 'ANY',
@@ -15,11 +19,30 @@ const netConfig:ConFig = {
   tokenListUrl: 'http://192.168.50.229:8110/tokenList/256',
   // tokenListUrl: 'https://list.smpcwallet.com/tokenList/256',
   // router: '0x2721b8e2A0f0939a289d4379e85fFf59F9226420',
-  router: '0xec00a74ff12a4c7cadc4596bd0278eafaebcc59d',
+  router: '0x479ab92bf721de918f01d455e90540149dbfd9da',
   lookHash: explorerUrl + '/tx/',
   lookAddr: explorerUrl + '/address/',
   lookBlock: explorerUrl + '/block/',
   tokenList: httest
+}
+
+if (ENV === 'main') {
+  explorerUrl = 'https://scan.hecochain.com'
+  netConfig = {
+    oldAppName: 'Anyswap V1',
+    appName: 'HTswap LP',
+    name: 'Huobi',
+    symbol: 'HT',
+    baseCurrency: 'ANY',
+    nodeRpc: 'https://http-mainnet.hecochain.com',
+    chainID: 128,
+    tokenListUrl: '',
+    router: '0x479ab92bf721de918f01d455e90540149dbfd9da',
+    lookHash: explorerUrl + '/tx/',
+    lookAddr: explorerUrl + '/address/',
+    lookBlock: explorerUrl + '/block/',
+    tokenList: htmain
+  }
 }
 
 const config: ConFig = {
